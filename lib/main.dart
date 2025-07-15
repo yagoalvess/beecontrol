a08db2503edc59ddaf00a36684774e66ef9d1f4d
Yago Alves <yagoalves455@gmail.com>
2025-07-14 21:33:57 -0300

    color app
    adição de coloração no app main principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
+import 'package:abelhas/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BeeControl',
+      theme: ThemeData(
+        primaryColor: Colors.amber, // cor principal do app
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber, // afeta AppBar, botões, etc.
+          brightness: Brightness.light, // ou Brightness.dark para tema escuro
+        ),
+        scaffoldBackgroundColor: Colors.yellow[50], // fundo da tela
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.amber, // cor da AppBar
+          foregroundColor: Colors.black, // cor do texto da AppBar
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+          bodyMedium: TextStyle(color: Colors.black),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
